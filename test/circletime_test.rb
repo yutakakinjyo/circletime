b16fd75eae5a88e4da22d490bc8d98f291cb1e52
yutakakinjyo <yutakakinjyo@gmail.com>
2015-12-11 02:25:50 +0900

    add sample test

--- test/circletime_test.rb
+++ test/circletime_test.rb
@@ -1,11 +1,18 @@
 require 'test_helper'
 
-class CircletimeTest < Minitest::Test
+class CircleTimeTest < Minitest::Test
+
   def test_that_it_has_a_version_number
     refute_nil ::Circletime::VERSION
   end
 
-  def test_it_does_something_useful
-    assert false
+  def test_it_week_build_timen
+    DateTime.stubs(:now).returns(DateTime.parse("Fri, 11 Dec 2015 02:20:15 +0900"))
+    res = OpenStruct.new
+    res.body = [{start_time:"2014-04-12T10:29:59.587Z", "build_time_millis":16308}]
+    CircleCi.stubs(:organization).returns(res)
+    build_t = CircleTime::BuildTime.new("yutakakinjyo")
+    p build_t.week
   end
+  
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,3 +2,5 @@ $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'circletime'
 
 require 'minitest/autorun'
+require "minitest/unit"
+require "mocha/mini_test"
